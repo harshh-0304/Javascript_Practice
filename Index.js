597ba26f59ac558ec59c4627ba4de35c1c32585d
harshh <bakasakuta33@gmail.com>
2025-06-29 23:18:06 +0530

    For loop

--- Index.js
+++ Index.js
@@ -1,10 +1,13 @@
-function check_even_or_odd(num) {
-if (num % 2 == 0){
-    return"even" ;
-}else{
+// function check_even_or_odd(num) {
+// if (num % 2 == 0){
+//     return"even" ;
+// }else{
 
-    return "odd";
-}
-}
+//     return "odd";
+// }
+// }
 
-console.log(check_even_or_odd(120));
+// console.log(check_even_or_odd(120));
+for (let i = 0; i <= 5; i++) {
+    console.log(i); // This will log 0 to 5
+}
