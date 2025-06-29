f0bec5a8a42fbb0c8edc1247a6b782985fccf721
harshh <bakasakuta33@gmail.com>
2025-06-29 23:26:30 +0530

    Area of reactangle

--- Index.js
+++ Index.js
@@ -1,29 +1,36 @@
-// // function check_even_or_odd(num) {
-// // if (num % 2 == 0){
-// //     return"even" ;
-// // }else{
+// // // function check_even_or_odd(num) {
+// // // if (num % 2 == 0){
+// // //     return"even" ;
+// // // }else{
 
-// //     return "odd";
+// // //     return "odd";
+// // // }
+// // // }
+
+// // // console.log(check_even_or_odd(120));
+// // for (let i = 0; i <= 5; i++) {
+// //     console.log(i); // This will log 0 to 5
 // // }
+
+
+// // let i = 5 ;
+// // while (i <= 5 ){
+// //     console.log(i)
+// //     i++ ;
 // // }
 
-// // console.log(check_even_or_odd(120));
-// for (let i = 0; i <= 5; i++) {
-//     console.log(i); // This will log 0 to 5
-// }
+//  function Add_numbers(a ,b){
 
 
-// let i = 5 ;
-// while (i <= 5 ){
-//     console.log(i)
-//     i++ ;
-// }
-
- function Add_numbers(a ,b){
+//         return a + b ;
+//  }
+// let sum = Add_numbers(7,9);
+// console.log(sum);
 
 
-        return a + b ;
- }
-let sum = Add_numbers(7,9);
-console.log(sum);
+function Area_of_triangle(width , height){
+    return (width * height) ;
 
+}
+let area = Area_of_triangle(9 , 8);
+console.log(area) ;
