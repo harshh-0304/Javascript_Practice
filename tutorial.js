c32f8372c10aef7d0b40eea045a2290ee06883b3
harshh <bakasakuta33@gmail.com>
2025-07-13 15:32:36 +0530

    should we play ?

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,8 @@
 const prompt = require('prompt-sync')();
 const name = prompt("What is your name? ");
-console.log("Hello, " + name + "!");
+console.log("Hello, " + name + "welcome to our game !");
+
+
+const ShouldWePlay = prompt('do you want to play ?') ;
+
+
